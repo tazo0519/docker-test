3bd3029b1f3b1d49a1a990ec01eec0f1b171f239
tazo0519 <tazo0519@naver.com>
2025-12-01 22:07:26 +0900

    HelloController 추가

---
+++ src/main/java/tazo0519/dockertest/controller/HelloController.java
@@ -0,0 +1,13 @@
+package tazo0519.dockertest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping
+    public String hello() {
+        return "Hello Docker World!";
+    }
+}
