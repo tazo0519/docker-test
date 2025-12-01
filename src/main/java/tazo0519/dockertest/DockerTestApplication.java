f7f17818368a49eae1671e77621595c0320a5d9c
tazo0519 <tazo0519@naver.com>
2025-12-01 18:18:27 +0900

    Initialization

---
+++ src/main/java/tazo0519/dockertest/DockerTestApplication.java
@@ -0,0 +1,13 @@
+package tazo0519.dockertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerTestApplication.class, args);
+    }
+
+}
