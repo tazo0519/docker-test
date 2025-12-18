94de294b3d8df2967e1ba6dbb76251bd0ea84e83
tazo0519 <tazo0519@naver.com>
2025-12-19 00:20:52 +0900

    Spring Boot & Redis & MySQL 동시에 띄우기

---
+++ src/main/java/tazo0519/dockertest/config/RedisConfig.java
@@ -0,0 +1,22 @@
+package tazo0519.dockertest.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJacksonJsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import tools.jackson.databind.ObjectMapper;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericJacksonJsonRedisSerializer(new ObjectMapper()));
+        return template;
+    }
+}
--- src/main/java/tazo0519/dockertest/controller/HelloController.java
+++ src/main/java/tazo0519/dockertest/controller/HelloController.java
@@ -1,13 +1,19 @@
 package tazo0519.dockertest.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class HelloController {
+
+    private final RedisTemplate<String, String> redisTemplate;
 
     @GetMapping
     public String hello() {
+        redisTemplate.opsForValue().set("hello", "world");
         return "Hello Docker World!";
     }
 }
